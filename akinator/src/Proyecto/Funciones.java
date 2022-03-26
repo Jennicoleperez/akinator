ee7019b2f3b6ed3d3952434a17cf04681a8f1213
Jennifer Pérez <jennifer.perez@correo.unimet.edu.ve>
2022-03-25 20:36:01 -0400

    commit

--- akinator/src/Proyecto/Funciones.java
+++ akinator/src/Proyecto/Funciones.java
@@ -12,11 +12,13 @@ import java.io.PrintWriter;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Jennifer Pérez
- */
+
 public class Funciones {
+    
+    /**
+    * Método lee el archivo predeterminado
+    */
+    
     public String leer(){
         String linea;
         String datos_csv = "";
@@ -41,5 +43,57 @@ public class Funciones {
                     JOptionPane.showMessageDialog(null, "Error al leer los datos.");
         }
         return datos_csv ;  
+    }
+    
+    /**
+     * Método lee el archivo cargado de manera remota
+     */
+    
+    public String leerCsvCargado() {
+
+        String line;
+        String csv = ""; 
+
+        try {
+            JFileChooser fileC = new JFileChooser();
+            fileC.showOpenDialog(null);
+            File file = fileC.getSelectedFile();
+            if (!file.exists()) {
+                file.createNewFile(); //si el archivo no existe, lo crea
+            } else {
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                while ((line = br.readLine()) != null) {
+                    if (!line.isEmpty()) {
+                        csv += line + " ";
+                    }
+
+                }
+                br.close();
+                JOptionPane.showMessageDialog(null, "Lectura exitosa.");
+
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al leer");
+        }
+        return csv;
+    }
+    
+    /**
+     * Función que te devuelve un arreglo con todas las palabras del texto, sin caracteres especiales.
+     * 
+     * @param texto texto proveniente de leer el csv
+     * @return arreglo con las palabras
+     * 
+     */
+    public String[] soloPalabrasArray(String texto) {
+
+        texto = texto.replaceAll("[^A-Za-z0-9áéíóúÁÉÍÓÚüÜ ]", "").toLowerCase();
+        texto.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace("ü", "u");
+
+        String[] array = texto.split(" ");
+
+        return array;
+
     }
 }
