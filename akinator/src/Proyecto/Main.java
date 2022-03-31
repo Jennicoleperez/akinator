a4ad8243f14f0a9122dc32f83777093589707d18
Jennifer Pérez <jennifer.perez@correo.unimet.edu.ve>
2022-03-31 06:44:03 -0400

    commit

--- akinator/src/Proyecto/Main.java
+++ akinator/src/Proyecto/Main.java
@@ -20,50 +20,10 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-//    public static ArbolBinario leer() {
-//        String linea;
-//        String path = "test\\conocimientos.csv";
-//        File file = new File(path);
-//        ArbolBinario arbol;
-//
-//        try {
-//            if (!file.exists()) {
-//                file.createNewFile();
-//
-//            } else {
-//                FileReader fr = new FileReader(file);
-//                BufferedReader br = new BufferedReader(fr);
-//                
-//                String[] aux = br.readLine().split("; ");
-//                NodoBinario raiz = new NodoBinario(aux[0]);
-//                arbol = new ArbolBinario(raiz);
-//                raiz.setHijoIzq(new NodoBinario(aux[1]));
-//                raiz.setHijoDer(new NodoBinario(aux[2]));
-//          
-//                
-//                while ((linea = br.readLine()) != null) {
-//                    if (!linea.isEmpty()) {
-//                        String[] aux2 = linea.split("; ");
-//                        NodoBinario raizAux = arbol.buscar(aux2[0], raiz);
-//                        raizAux.setHijoIzq(new NodoBinario(aux2[1]));
-//                        raizAux.setHijoDer(new NodoBinario(aux2[2]));
-//                    }
-//                }
-//                return arbol;
-//            }
-//            
-//        } catch (Exception err) {
-//            JOptionPane.showMessageDialog(null, "Error al leer los datos.");
-//        }
-//        return null;
-//    }
+
 
     public static void main(String[] args) {
-//        Funciones f = new Funciones();
-////        ArbolBinario arbol = f.leerCsvCargado();
-////        
-////        arbol.preorden();
-//          
+         
           VentanaInicio vi = new VentanaInicio();  
 
     }
